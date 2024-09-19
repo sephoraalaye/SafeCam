ab07b3fecc1f1971b80ec8351e3f3e1b17c68861
sephoraalaye <alayesephora99@gmail.com>
2024-09-19 13:20:36 +0100

    version 1.2

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+window.addEventListener('scroll', function() {
+    reveal(50);
+});
+
+
+
+function reveal(revealpoint){
+    let reveals = document.querySelectorAll(".reveal");
+    for (let i = 0; i< reveals.length ; i++){
+        let  windowheight = window.innerHeight;
+        let revealtop = reveals[i].getBoundingClientRect().top;
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+
+
+
 container = document.getElementById("container");
 container.style.width=600+"%";
-
-
 const imagePaths = [
 'images/carousel-1.jpg',
 'images/carousel-2.jpg',
@@ -10,25 +31,15 @@ const imagePaths = [
 'images/carousel-2.jpg',
 'images/carousel-3.jpg',
 ];
-
 const totalImages = imagePaths.length;
 const blocDiv = document.getElementsByClassName("items");
-
-
-
-
+let currentIndex = 0;
+let direction = 1; // 1 pour avancer, -1 pour reculer
 for (var i=0;i<totalImages;i++){
     blocDiv[i].style.width = 100+"%";
     blocDiv[i].style.background = "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),"+` url(${imagePaths[i]})`;
     blocDiv[i].style.backgroundSize = "cover";
 }
-
-
-
-
-let currentIndex = 0;
-let direction = 1; // 1 pour avancer, -1 pour reculer
-
 
 function moveCarousel() {
     // alert(currentIndex);
@@ -40,15 +51,10 @@ function moveCarousel() {
         direction = -1; // Inverser la direction pour reculer
     } else if (currentIndex === 0) {
         direction = 1; // Revenir à l'avant
-    }
-
-    
+    }   
     currentIndex += direction;
-
-    
 }
 
-// Fonction pour démarrer le carrousel
 function startCarousel() {
     setInterval(() => {
         moveCarousel();
@@ -57,6 +63,12 @@ function startCarousel() {
 
 // Lancer le carrousel
 startCarousel();
+
+// ::::::FIN Carousel:::::://
+
+// window.addEventListener("scroll",reveal);
+
+// console.log(window.innerHeight);
 
 
 
