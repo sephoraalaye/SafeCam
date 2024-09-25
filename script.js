e842d53a6daaf310a15d8746e3f1d528ea07516d
sephoraalaye <alayesephora99@gmail.com>
2024-09-25 22:58:56 +0100

    j'ai fais testimonial et je viens de tomber sur le vrai site, c'est tellement magnifique. Donc je vais essayer de faire le vrai site. Reprenons alors le home. version 1.0001

--- script.js
+++ script.js
@@ -64,4 +64,73 @@ function reveal() {
 }
 
 
+const clients = document.querySelector(".clients");
+const lesclients = document.getElementsByClassName("client");
+const nextBtn = document.querySelector(".next");
+const dots = document.querySelectorAll(".dot");
+
+let index = 1;
+var width; 
+
+
+function slideWidth(){
+    width = lesclients[0].clientWidth + 38;
+}
+
+slideWidth();
+
+
+
+// window.addEventListener("resize",slideWidth);
+clients.style.transform = 'translateX('+(-width * index) +'px)';
+
+
+nextBtn.addEventListener("click",nextSlide);
+function nextSlide(){
+    if(index >= lesclients.length  - 1 ){return};
+    clients.style.transition = "transform 0.1ms ease";
+    index++;
+    clients.style.transform = 'translateX('+(-width * index) +'px)';
+    dotsLabel();
+}
+
+
+clients.addEventListener("transitionend",function(){
+    lesclients[index].className += ' active';
+    lesclients[index - 1].className = lesclients[index - 1].className.replace(' active','');
+    if(lesclients[index].id === 'firstImageDuplicate'){
+        lesclients[index - 1].className = lesclients[index - 1].className.replace(' active','');
+        lesclients[index].className += ' active';
+        clients.style.transition = "none";
+        index = lesclients.length -1 - index ;
+        clients.style.transform = 'translateX('+(-width * index) +'px)';
+        lesclients[index].className += ' active';
+        dotsLabel();
+    }
+    lesclients[5].className = lesclients[5].className.replace(' active','');
+
+})
+
+// Auto Sliding 
+
+function autoslide(){
+    deleteInterval = setInterval(timer,1000);
+    function timer(){
+        nextSlide();
+
+    }
+}
+autoslide();
+
+function dotsLabel(){            
+    for (let i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(' active','');
+    }
+    if(index > 4){
+        dots[index - 5].className += ' active';
+    }else{
+        dots[index - 1].className += ' active';
+    }
+}
+
 
