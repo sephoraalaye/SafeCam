d70f4272fde609ca64c6ec862e66f19cfde4c5d2
sephoraalaye <alayesephora99@gmail.com>
2024-10-05 14:12:07 +0100

    version 0.001

--- script.js
+++ script.js
@@ -1,45 +1,8 @@
 container = document.getElementById("container");
-container.style.width=600+"%";
-const imagePaths = [
-'images/carousel-1.jpg',
-'images/carousel-2.jpg',
-'images/carousel-3.jpg',
-'images/carousel-1.jpg',
-'images/carousel-2.jpg',
-'images/carousel-3.jpg',
-];
-const totalImages = imagePaths.length;
-const blocDiv = document.getElementsByClassName("items");
-let currentIndex = 0;
-let direction = 1; // 1 pour avancer, -1 pour reculer
-for (var i=0;i<totalImages;i++){
-    blocDiv[i].style.width = 100+"%";
-    blocDiv[i].style.background = "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),"+` url(${imagePaths[i]})`;
-    blocDiv[i].style.backgroundSize = "cover";
-}
 
-function moveCarousel() {
-    // alert(currentIndex);
-    const translateValue = -currentIndex * 100 / totalImages; 
-    container.style.transform = `translate(${translateValue}%)`;
-    container.style.transition = "all 0.5s ease 0.2s";
-
-    if (currentIndex === totalImages - 1) {
-        direction = -1; // Inverser la direction pour reculer
-    } else if (currentIndex === 0) {
-        direction = 1; // Revenir à l'avant
-    }   
-    currentIndex += direction;
-}
-
-function startCarousel() {
-    setInterval(() => {
-        moveCarousel();
-    }, 1000);
-}
 
 // Lancer le carrousel
-startCarousel();
+// startCarousel();
 
 // ::::::FIN Carousel:::::://
 
@@ -50,6 +13,18 @@ reveal();
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
     var windowHeight = window.innerHeight;
+    const header = document.querySelector("header");
+    const main = document.querySelector("main");
+    var mainTop = main.getBoundingClientRect().top;
+
+    if (mainTop < -20 ){
+        header.classList.add('smallheight');
+    }else{
+        header.classList.remove('smallheight');
+    }
+
+    console.log(header.style.height);
+
 
     reveals.forEach(function(reveal) {
         var revealPoint = parseInt(reveal.getAttribute("data-reveal-point"), 10);
